864897913ef49e7f508bf21036da9aa70eddc8b8
utkarshanand <anand.utkarsh96@gmail.com>
2017-12-01 17:45:20 -0500

    Create udpReciever.py
    UDP reciever

---
+++ SmartParkingMonitor/udpReciever.py
@@ -0,0 +1,22 @@
+import socket, sys, time, json
+
+textport = 5047
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = ("", port)
+s.bind(server_address)
+
+while True:
+
+    #print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+
+    buf, address = s.recvfrom(2048)
+    if not len(buf):
+        break
+    #print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+    data = json.loads(buf)
+    #print (data)
+    print (data["car arrived"])
+    
+s.shutdown(1)
