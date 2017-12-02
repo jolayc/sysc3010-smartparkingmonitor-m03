b0d2b232958c78e0810557abfb3a12b5d3640983
jolayc <32461067+jolayc@users.noreply.github.com>
2017-12-01 19:05:14 -0500

    Update Main.py
    Added: Send JSON String over socket

--- SmartParkingMonitor/Main.py
+++ SmartParkingMonitor/Main.py
@@ -1,8 +1,13 @@
 # https://github.com/jolayc/sysc3010-smartparkingmonitor/edit/master/SmartParkingMonitor/Main.py
 # @author Osama Rachid
 
-import serial, time, datetime, socket
+import serial, time, datetime, socket, sys, json
 from MonitoringNode import MonitoringNode # general utility class
+
+host = "10.0.0.32" # TO DO
+socketport = "5047"
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = (host, socketport)
 
 port = '/dev/ttyACM0' # port where arduino is connected
 baud = 9600 # arduino baud rate for serial communication
@@ -15,7 +20,14 @@ A = MonitoringNode(0,0,False,8)
 B = MonitoringNode(1,0,False,8)
 C = MonitoringNode(2,0,False,8)
 
-data = {}
+oldData = {"car arrived A": False,
+        "is overlimit A": False,
+        "car arrived B": False,
+        "is overlimit B": False,
+        "car arrived C": False,
+        "is overlimit C": False,
+      }
+changes = [None, None, None, None, None, None]
 
 while True:
     ser.flush() # refresh serial port
@@ -32,10 +44,34 @@ while True:
         A.setDistance(arduino_readings[1])
         B.setDistance(arduino_readings[3])
         C.setDistance(arduino_readings[5])
-
-        A.update()
-        B.update()
-        C.update()
-
-       # print(int(A.getTime()), " ", int(B.getTime()), " ", int(C.getTime()))
-       # TO-DO: send data to server Pi
+ 
+        data = {"A1": A.carArrived(),
+                "O1": A.isOverlimit(),
+                "A2": B.carArrived(),
+                "O2": B.isOverlimit(),
+                "A3": C.carArrived(),
+                "O3": C.isOverlimit(),
+        }
+    
+        newData = data
+        
+        for key in newData:
+            if(newData[key] != oldData[key]):
+                if(key == "A1"):
+                    changes[0] = new[key]
+                if(key == "A2"):
+                    changes[2] = new[key]
+                if(key == "A3"):
+                    changes[4] = new[key]
+                if(key == "O1"):
+                    changes[1] = new[key]
+                if(key == "O2"):
+                    changes[3] = new[key]
+                if(key == "O3"):
+                    changes[5] = new[key]
+             JSONString = json.dumps(changes)
+             s.sendto(string.encode('utf-8'), server_address)
+               
+        oldData = data
+        
+s.shutdown(1)
