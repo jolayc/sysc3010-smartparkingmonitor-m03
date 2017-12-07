9ddcd38fb435b02df9ba1601c746bc865c9e3271
jolayc <32461067+jolayc@users.noreply.github.com>
2017-12-07 12:01:18 -0500

    Update UDPSender.py
    Added description, this file was not used in final system

--- SmartParkingMonitor/UDPSender.py
+++ SmartParkingMonitor/UDPSender.py
@@ -1,7 +1,8 @@
 # Example fo UDPSender to include with Main.py
+# This code was used as a guide, it wasn't actually used in our system
 import socket, sys, time, json
 
-host = "134.117.57.197" # replace with static IP of pi
+host = "134.117.57.197" # replace with static IP of pi, this IP was for a lab machine
 textport = "5047" # replace with port number to be used
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
